7d6400d8e80e0f1768ec9087bf7edcc0731ba5ab
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-07-19 08:27:28 +0100

    chamge password update

--- src/components/DashBoardNav.vue
+++ src/components/DashBoardNav.vue
@@ -6,10 +6,32 @@
            
           <div class="relative mr-20 ">
               <ol class="flex text-sm font-normal">
-                <li class="block px-4 py-2 hover:bg-gray-400 hover:text-blue-900 cursor-pointer">Change Password</li>
+                <li @click="toggleChangePassword" class="block px-4 py-2 hover:bg-gray-400 hover:text-blue-900 cursor-pointer">Change Password</li>
               <li class="block px-4 py-2 hover:bg-gray-400 hover:text-blue-900 cursor-pointer" @click="logout">Logout</li>
               </ol>
           </div>
+      </div>
+
+      <div v-if="showChangePassword" class="modal-overlay bg-gray-400 rounded">
+        
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-lg font-bold leading-6 text-blue-900">Password</label>
+            <div class="text-sm">
+              <!--<a href="#" class="font-semibold text-indigo-100 hover:text-blue-50">Forgot password?</a>-->
+            </div>
+          </div>
+          <div class="mt-2 mb-3">
+            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" 
+            class="block w-full rounded-md border-0 py-1.5 px-2 text-blue-900 font-bold shadow-sm ring-1 ring-inset
+             ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 
+             sm:text-sm sm:leading-6" placeholder="Enter New Password" />
+          </div>
+          <div class="flex">
+            <button class="inline-flex w-40 mr-5 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm">Enter</button>
+            <button @click="closePassword" class="mt-5 sm:mt-0  inline-flex justify-center w-40 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm">Cancel</button>
+          </div>
+        </div>
       </div>
   
      <div class="settings-nav text-gray-100 h-[94vh]">
@@ -33,10 +55,17 @@
         name: 'DashBoardNav',
         data() {
           return {
-            isOpen: false,
+            showChangePassword: false, // Flag to control the edit modal visibility
           };
         },
         methods: {
+          toggleChangePassword() {
+        this.showChangePassword = !this.showChangePassword;
+      },
+
+      closePassword(){
+        this.showChangePassword = false;
+      },
 
           logout() {
       // Remove the login data from localStorage
@@ -52,6 +81,18 @@
     <style scoped>
   .logo{
     width: 40px;
+  }
+
+  .modal-overlay {
+    position: fixed;
+    z-index: 9999;
+    width: 30%;
+    height: 30vh;
+    margin-left: 60%;
+    margin-top: 1%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
   .content{
     position: relative;
