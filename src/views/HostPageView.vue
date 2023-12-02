bd8f01f3966a859d958981e52194140ee3c0e031
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-12-02 13:57:35 +0100

    update

--- src/views/HostPageView.vue
+++ src/views/HostPageView.vue
@@ -14,7 +14,7 @@
         </div>
         <div
           class="bg-green-800 w-40 text-lg font-bold text-gray-200 rounded mt-3 ml-32 text-center hover:bg-green-600 cursor-pointer"
-          @click="revealAnswer">
+          @click="revealAnswer" >
           Reveal Answer
         </div>
       </div>
